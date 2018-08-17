4e678ef561e19208976701b15e17ff86e879f220
Lucas Spigariol <lspigariol@gmail.com>
2018-08-17 14:23:28 -0300

    Create solucion.pl

---
+++ solucion.pl
@@ -0,0 +1,46 @@
+
+%originarioDe(Genero,Pais).
+originarioDe(tango, argentina).
+originarioDe(chacarera, argentina)
+originarioDe(enka, japon).
+originarioDe(deathMetalMelodico, suecia).
+originarioDe(celta, europaOccidental).
+originarioDe(popLatino, latinoamerica).
+
+
+%pais(Pais,Region).
+pais(noruega, escandinavia).
+pais(dinamarca, escandinavia).
+pais(suecia, escandinavia).
+pais(suecia, europaOccidental).
+pais(alemania, europaOccidental).
+pais(argentina, latinoamerica).
+pais(colombia, latinoamerica).
+pais(argentina, tercerMundo).
+
+%produccion(Artista, Trabajo)
+produccion(piazzolla, disco(tango,10,acmeMusic)). 
+produccion(goyeneche, disco(tango,10,acmeMusic)). 
+produccion(leonGieco, disco(rock, 12, utnMusic)).
+produccion(darkTranquility, recital(argentina, [deathMetalMelodico, celta])).
+produccion(faun, disco(folk, 45, deustcheMusic)).
+produccion(faun, homenaje(goyeneche)).
+produccion(enka, homenaje(mercedesSosa)).
+produccion(enka, recital(japon, [sayuriIshikawa])).
+produccion(shakira, disco(popLatino, 10, antonitoMusic)).
+produccion(shakira, disco(popLatino, 5, piqueMusic)).
+produccion(mercedesSosa, disco(chacarera, 20, acmeMusic)).
+
+%artista(Artista, Pais)
+artista(piazzolla, argentina).
+artista(leonGieco, argentina).
+artista(darkTranquility, suecia).
+artista(faun, alemania).
+artista(enka, japon).
+artista(shakira, colombia).
+artista(mercedesSosa, argentina).
+artista(goyeneche, polonia). %sic
+
+%discografica(Discografica,Pais)
+discografica(utnMusic, argentina).
+discografica(deustcheMusic, alemania).
